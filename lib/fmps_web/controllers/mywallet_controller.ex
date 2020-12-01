0eb930a4f430bc9f140d282bc6cfa3075f2f1496
Karina Karimova <kkarim@ttu.ee>
2020-12-01 10:43:26 +0200

    TDD feature/FMPS-36 [Failing]

--- lib/fmps_web/controllers/mywallet_controller.ex
+++ lib/fmps_web/controllers/mywallet_controller.ex
@@ -14,11 +14,15 @@ defmodule FmpsWeb.MywalletController do
     def update(conn, %{"input_money" => input_money}) do
       
       user = Fmps.Authentication.load_current_user(conn)
-      changeset = Changeset.change(user, %{balance: user.balance + elem(Float.parse(input_money),0)})
-      newData = changeset |> Repo.update!()
-      IO.inspect newData
-      render(conn, "mywallet.html", user: newData, changeset: changeset)
+      first = elem(Float.parse(input_money),0)
+      if is_number(first) && first>0 do
+        changeset = Changeset.change(user, %{balance: user.balance + first})
+        newData = changeset |> Repo.update!()
+        render(conn, "mywallet.html", user: newData, changeset: changeset)
+      else 
+        conn
+        |> put_flash(:info, "Invalid input")
+        |> redirect(to: Routes.mywallet_path(conn, :index))
+      end
     end
-
-
 end
---
+++ test/fmps_web/controllers/mywallet_controller_test.exs
@@ -0,0 +1,54 @@
+defmodule FmpsWeb.MywalletControllerTest do
+    use FmpsWeb.ConnCase
+    use Hound.Helpers
+
+    alias Fmps.{Repo, Accounts.User}
+    alias Fmps.Guardian
+    
+    setup do
+      user =
+        Repo.insert!(%User{
+        name: "Anna Karenina",
+        email: "anna.karenina@gmail.com",
+        licence_number: "ES345632",
+        password: "parool"
+      })
+      conn = guardian_login(user)
+      {:ok, conn: conn}
+    end
+
+    @valid_input("12")
+
+    @invalid_input("-1")
+    @input_input1("abc")
+
+    def guardian_login(user) do
+        build_conn()
+        |> bypass_through(Takso.Router, [:browser, :browser_authenticated_session])
+        |> get("/")
+        |> Map.update!(:state, fn _ -> :set end)
+        |> Guardian.Plug.sign_in(user)
+        |> send_resp(200, "Flush the session")
+        |> recycle
+    end
+
+    describe "money input" do
+        test "Adds money to balance", %{conn: conn} do 
+          conn = put(conn, Routes.mywallet_path(conn, :update, 0), input_money: @valid_input)
+          assert html_response(conn, 200) =~ ~r/12.0/
+        end
+    
+        test "returns error in case of negative input ", %{conn: conn} do
+            conn = put(conn, Routes.mywallet_path(conn, :update, 0), input_money: @invalid_input)
+            conn = get conn, redirected_to(conn)
+            assert html_response(conn, 200) =~ ~r/Invalid input/
+        end
+
+        test "returns error in case of string input ", %{conn: conn} do
+            conn = put(conn, Routes.mywallet_path(conn, :update, 0), input_money: @invalid_input1)
+            conn = get conn, redirected_to(conn)
+            assert html_response(conn, 200) =~ ~r/Invalid input/
+        end
+    end
+end
+    
