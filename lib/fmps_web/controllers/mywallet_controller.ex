1e3cbe04a575e01e99b5861f3e955bd57ec85954
Karina Karimova <kkarim@ttu.ee>
2020-12-01 10:53:44 +0200

    TDD feature/FMPS-36 [Passing]

--- lib/fmps_web/controllers/mywallet_controller.ex
+++ lib/fmps_web/controllers/mywallet_controller.ex
@@ -14,15 +14,22 @@ defmodule FmpsWeb.MywalletController do
     def update(conn, %{"input_money" => input_money}) do
       
       user = Fmps.Authentication.load_current_user(conn)
-      first = elem(Float.parse(input_money),0)
-      if is_number(first) && first>0 do
-        changeset = Changeset.change(user, %{balance: user.balance + first})
-        newData = changeset |> Repo.update!()
-        render(conn, "mywallet.html", user: newData, changeset: changeset)
-      else 
-        conn
-        |> put_flash(:info, "Invalid input")
-        |> redirect(to: Routes.mywallet_path(conn, :index))
+    
+      try do
+        first = elem(Float.parse(input_money),0)
+        if is_number(first) && first>0 do
+          changeset = Changeset.change(user, %{balance: user.balance + first})
+          newData = changeset |> Repo.update!()
+          render(conn, "mywallet.html", user: newData, changeset: changeset)
+        else 
+          conn
+          |> put_flash(:info, "Invalid input")
+          |> redirect(to: Routes.mywallet_path(conn, :index))
+        end
+      rescue
+        _ -> conn
+             |> put_flash(:info, "Invalid input")
+             |> redirect(to: Routes.mywallet_path(conn, :index))
       end
     end
 end
