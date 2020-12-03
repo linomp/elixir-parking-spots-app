04205b73f34385cf3cc0eb3ab12ee21601acff5d
Mike Camara <migome@ttu.ee>
2020-12-03 21:18:02 +0200

    [Failing]

---
+++ lib/fmps_web/controllers/settings_controller.ex
@@ -0,0 +1,32 @@
+defmodule FmpsWeb.SettingsController do
+  use FmpsWeb, :controller
+  alias Fmps.Repo
+  alias Fmps.Accounts.User
+  alias Ecto.{Changeset, Multi}
+
+  def index(conn, _params) do
+    user = Fmps.Authentication.load_current_user(conn)
+    changeset = User.changeset(user, %{})
+    render(conn, "settings.html", user: user, changeset: changeset)
+  end
+
+  def update(conn, %{"set_payment_method" => set_payment_method}) do
+    user = Fmps.Authentication.load_current_user(conn)
+    try do
+        elemPaymentMethod = elem(Float.parse(set_payment_method),0)
+        if is_number(elemPaymentMethod) && elemPaymentMethod>0 do
+          changeset = Changeset.change(user, %{is_monthly_payment: user.is_monthly_payment })
+          newData = changeset |> Repo.update!()
+          render(conn, "settings.html", user: newData, changeset: changeset)
+        else
+          conn
+          |> put_flash(:info, "Invalid input")
+          |> redirect(to: Routes.settings_path(conn, :index))
+        end
+      rescue
+        _ -> conn
+             |> put_flash(:info, "Invalid input")
+             |> redirect(to: Routes.settings_path(conn, :index))
+    end
+  end
+end
