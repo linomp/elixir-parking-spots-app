04a5646f590912fdddc81bbd388382f342133bea
Elen Tumasyan <eltuma@ttu.ee>
2020-12-04 03:44:40 +0200

    BDD feature/FMPS-45 [Failing]

---
+++ lib/fmps_web/controllers/ongoing_booking_contoller.ex
@@ -0,0 +1,37 @@
+defmodule FmpsWeb.OngoingBookingController do
+  use FmpsWeb, :controller
+  import Ecto.Query
+
+  alias Fmps.Repo
+  alias Fmps.Sales.{Booking}
+
+  def index(conn, _params) do
+    
+    user = Fmps.Authentication.load_current_user(conn)
+    query = from b in Booking, where: b.user_id == ^user.id and b.is_finished == false, order_by: [desc: b.inserted_at], limit: 1
+    booking = Repo.one(query)
+    if booking !== nil do
+      render(conn, "index.html", ongoingExists: true, isHourly: booking.is_hourly)
+    else
+      render(conn, "index.html", ongoingExists: false, isHourly: false)
+    end
+  end
+
+  def create(conn, _params) do
+    user = Fmps.Authentication.load_current_user(conn)
+    query = from b in Booking, where: b.user_id == ^user.id and b.is_finished == false, order_by: [desc: b.inserted_at], limit: 1
+    booking = Repo.one(query)
+    
+    if !booking.is_hourly do
+      conn
+      |> put_flash(:info, "Pay")
+      |> render("index.html", ongoingExists: true, isHourly: booking.is_hourly)
+    else 
+      conn
+      |> put_flash(:info, "Extend")
+      |> render("index.html", ongoingExists: true, isHourly: booking.is_hourly)
+    end
+
+  end
+
+end
